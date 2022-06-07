f359472086470b01948d4fe5abfe4923f1322843
Ryan Weeks <RyanWeeks@Ryans-MacBook-Pro.local>
2022-06-07 16:24:26 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+
+
+        As soon as I save the file, <code>src/App.js</code> the localhost is already changed!
+
+
         </p>
         <a
           className="App-link"
